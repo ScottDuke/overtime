4259b91161ae6efe622463c576922c920794b18c
Scott Duke <scottdookey@gmail.com>
2019-08-20 12:18:13 +0100

    added user validations for first and last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "test", last_name: "testy")
+    end
+
+    it "creates a user" do
+      expect(@user).to be_valid  
+    end
+
+    context "is not valid when" do
+      it "first_name is empty" do
+        @user.first_name = nil
+        expect(@user).to_not be_valid
+      end
+
+       it "last_name is empty" do
+        @user.last_name = nil
+        expect(@user).to_not be_valid
+      end
+    end
+    
+  end
 end
