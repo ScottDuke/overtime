2500e197267c3ee58fa22af89b58ae1d1752274a
Scott Duke <scottdookey@gmail.com>
2019-08-20 10:56:40 +0100

    added test for static homepage

---
+++ spec/features/static_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "navigate" do
+  describe "homepage" do
+
+    it "loads the page successfully" do
+      visit root_path
+      expect(page.status_code).to eql(200)  
+    end
+    
+  end
+end
