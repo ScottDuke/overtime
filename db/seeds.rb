c3611d541d321e4ed6ba56f5849e664fe3bd3df7
Scott Duke <scottdookey@gmail.com>
2019-08-21 11:45:46 +0100

    Added user to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+puts "creating a user"
+
+@user = User.create(email: "test@example.com", password: "password", password_confirmation: "password", first_name: "Testy", last_name: "Tester")
+
+puts "Created user"
+
 puts "creating 100 posts"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} content")
+  Post.create!(date: Date.today, rationale: "#{post} content", user: @user)
 end
 
 puts "finished creating 100 posts"
