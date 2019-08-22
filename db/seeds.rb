aa4d43207bef835759743f47607b5eb21d0d953c
Scott Duke <scottdookey@gmail.com>
2019-08-22 18:27:08 +0100

    Updated seeds.rb to create admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ puts "creating a user"
 
 puts "Created user"
 
+AdminUser.create(email: "admin@example.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "Tester")
+
+puts "Created admin user"
+
 puts "creating 100 posts"
 
 100.times do |post|
