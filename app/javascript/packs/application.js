b00671f836d76d375e4eb706e96a6952c9ae87ac
Scott Duke <scottdookey@gmail.com>
2019-08-22 08:52:23 +0100

    Added toastify for flash notices

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,9 @@ require("channels")
 
 import '../stylesheets/application'
 import './bootstrap_custom.js'
+import Toastify from 'toastify-js'
 
+window.Toastify = Toastify;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
 const webpack = require('webpack')
 environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default'],
+    Toastify: 'toastify-js/src/toastify'
   })
 )
+
+module.exports = environment
