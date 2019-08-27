af0de2b1f67629559d3d6619efd9f8e8b7c25f85
Scott Duke <scottdookey@gmail.com>
2019-08-27 11:36:51 +0100

    Added the state of the post to the index page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+  def state_badge(state)
+    badge_class = case state
+    when "submitted"
+      "primary"
+    when "approved"
+      "success"
+    when "rejected"
+      "danger"
+    end
+
+    content_tag(:span, state.titleize, class: "badge badge-#{badge_class}")
+  end
+  
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -11,4 +11,18 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PostsHelper, type: :helper do
+
+  describe "#state_badge" do
+    it "returns a primary badge when state is submitted" do
+      expect(self.state_badge("submitted")).to eq("<span class=\"badge badge-primary\">Submitted</span>")
+    end
+
+    it "returns a danger badge when state is rejected" do
+      expect(self.state_badge("rejected")).to eq("<span class=\"badge badge-danger\">Rejected</span>")
+    end
+
+    it "returns a success badge when state is approved" do
+      expect(self.state_badge("approved")).to eq("<span class=\"badge badge-success\">Approved</span>")
+    end
+  end
 end
