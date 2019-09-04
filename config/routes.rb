1e5d129050a88de17f981aec95013c03f91c23c3
Scott Duke <scottdookey@gmail.com>
2019-09-04 12:26:39 +0100

    Updated to restrict resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts
-  resources :audit_logs
+  resources :audit_logs, except: %i(new, edit, destroy)
 
   devise_for :users, skip: %i(registrations)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
