183452867fad8905f2cad5f8f378640dca88f7c3
Scott Duke <scottdookey@gmail.com>
2020-04-28 15:31:47 +0100

    Updated employee dashboard for what is searchable

--- app/dashboards/employee_dashboard.rb
+++ app/dashboards/employee_dashboard.rb
@@ -10,25 +10,25 @@ class EmployeeDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    posts: Field::HasMany,
-    audit_logs: Field::HasMany,
-    id: Field::Number,
-    email: Field::String,
-    encrypted_password: Field::String,
-    reset_password_token: Field::String,
-    reset_password_sent_at: Field::DateTime,
-    remember_created_at: Field::DateTime,
-    sign_in_count: Field::Number,
-    current_sign_in_at: Field::DateTime,
-    last_sign_in_at: Field::DateTime,
+    posts: Field::HasMany.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    email: Field::String.with_options(searchable: true, searchable_field: 'email'),
+    phone_number: Field::String.with_options(searchable: true, searchable_field: 'phone_number'),
+    password: Field::String.with_options(searchable: false),
+    password_confirmation: Field::String.with_options(searchable: false),
+    reset_password_token: Field::String.with_options(searchable: false),
+    reset_password_sent_at: Field::DateTime.with_options(searchable: false),
+    remember_created_at: Field::DateTime.with_options(searchable: false),
+    sign_in_count: Field::Number.with_options(searchable: false),
+    current_sign_in_at: Field::DateTime.with_options(searchable: false),
+    last_sign_in_at: Field::DateTime.with_options(searchable: false),
     current_sign_in_ip: Field::String.with_options(searchable: false),
     last_sign_in_ip: Field::String.with_options(searchable: false),
-    first_name: Field::String,
-    last_name: Field::String,
-    type: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
-    phone_number: Field::String,
+    first_name: Field::String.with_options(searchable: false),
+    last_name: Field::String.with_options(searchable: false),
+    type: Field::String.with_options(searchable: true, searchable_field: 'type'),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -38,22 +38,19 @@ class EmployeeDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     posts
-    audit_logs
     id
     email
+    password
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     posts
-    audit_logs
-    id
     email
-    encrypted_password
-    reset_password_token
+    phone_number
+    password
     reset_password_sent_at
-    remember_created_at
     sign_in_count
     current_sign_in_at
     last_sign_in_at
@@ -64,29 +61,19 @@ class EmployeeDashboard < Administrate::BaseDashboard
     type
     created_at
     updated_at
-    phone_number
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    posts
-    audit_logs
     email
-    encrypted_password
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
-    sign_in_count
-    current_sign_in_at
-    last_sign_in_at
-    current_sign_in_ip
-    last_sign_in_ip
+    phone_number
+    password
+    password_confirmation
     first_name
     last_name
     type
-    phone_number
   ].freeze
 
   # COLLECTION_FILTERS
@@ -97,14 +84,14 @@ class EmployeeDashboard < Administrate::BaseDashboard
   # in the search field:
   #
   #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
+  #     open: ->(resources) { where(open: true) }
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how employees are displayed
+  # Overwrite this method to customize how admin users are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(employee)
-  #   "Employee ##{employee.id}"
+  # def display_resource(admin_user)
+  #   "AdminUser ##{admin_user.id}"
   # end
 end
